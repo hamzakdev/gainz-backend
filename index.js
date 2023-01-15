962515e6b7a77dd808e7b24efb2eadd616d65092
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-15 00:23:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const dotenv = require('dotenv').config()
 const productRoute = require('./routes/product')
 const userRoute = require('./routes/user')
-const orderRoute = require('./routes/order')
 const connectDatabase = require('./db')
 const ErrorMiddleware = require('./middlewares/error')
 const AsyncErrorsMiddleware = require('./middlewares/asyncErrors');
@@ -30,7 +29,6 @@ app.use(cookieParser())
 
 app.use('/api/products', productRoute);
 app.use('/api/users', userRoute);
-app.use('/api/orders', orderRoute);
 
 
 //error handling :
