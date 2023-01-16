801bd27cf9fba3b4cdedb3d183535a919d0a4b91
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 15:13:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDatabase().then(app.listen(process.env.PORT, ()=>{
     console.log('server is running')
 }))
            app.use((req,res,next)=>{
-        res.header('Access-Control-Allow-Origin', 'https://gainz-ecommerce.web.app');
+        res.header('Access-Control-Allow-Origin', 'https://gainz-ecommerce.web.app/');
         res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
