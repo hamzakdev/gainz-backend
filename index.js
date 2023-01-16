da07359dd09935dee54650949415cfcb61b781b8
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 15:42:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 
 
-app.use(cors({credentials:true, origin:true}))
 //connecting to database : 
 connectDatabase().then(app.listen(process.env.PORT, ()=>{
     console.log('server is running')
