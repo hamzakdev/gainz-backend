a004cf103001fe8b272f00557180662bff7b9caa
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 15:40:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.use(cors({credentials:true, origin:true}))
 connectDatabase().then(app.listen(process.env.PORT, ()=>{
     console.log('server is running')
 }))
-//            app.use((req,res,next)=>{
+           app.use((req,res,next)=>{
 //         res.header('Access-Control-Allow-Origin', 'https://gainz-ecommerce.web.app');
 //         res.header('Access-Control-Allow-Credentials', true);
-// //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-//     next()
-//     })
+    next()
+    })
 
 
 
