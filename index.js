fb514eb5ae8838e91ab6c71dc8e0455b8ef8bbfa
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 13:11:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cors({
   "origin": "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
-  "optionsSuccessStatus": 204
+  "optionsSuccessStatus": 204,
+    allowedHeaders: "content-type"
 }))
 
 
