a1248cd856e56063834b3619a0008375c36f14fb
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 14:26:51 -0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -22,7 +22,7 @@ router.post(
     });
     const token = user.getJWTToken();
     const options = {
-//       httpOnly: true,
+      httpOnly: true,
       expires: new Date(Date.now() + 86400000 * 5),
     };
 
@@ -60,7 +60,7 @@ router.post(
     const token = user.getJWTToken();
 
     const options = {
-      httpOnly: true,
+//       httpOnly: true,
       expires: new Date(Date.now() + 86400000 * 5),
     };
 	const {isAdmin} = user._doc
