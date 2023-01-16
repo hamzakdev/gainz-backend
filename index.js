67d7e75be7f729c215a5873153fe4bf6e898b784
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 13:47:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const cors = require('cors')
 connectDatabase().then(app.listen(process.env.PORT, ()=>{
     console.log('server is running')
 }))
- app.use((req,res,next)=>{
+  app.use((req,res,next)=>{
         res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//         res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next()
     })
