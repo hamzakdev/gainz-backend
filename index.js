07088487cd71d9bb5148dd3217132ee0468b9286
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 13:44:12 -0800

    CORS

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDatabase().then(app.listen(process.env.PORT, ()=>{
 }))
  app.use((req,res,next)=>{
         res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-        res.header('Access-Control-Allow-Credentials', true);
+//         res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
