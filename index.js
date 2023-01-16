e90304dc5c93faba672f498121f331417d035718
Hamza Dev <99216609+hamzakdev@users.noreply.github.com>
2023-01-16 12:04:55 -0800

    SOLVING CORS

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ connectDatabase().then(app.listen(process.env.PORT, ()=>{
     console.log('server is running')
 }))
 app.use(cors())
-app.all('*', function (req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-})
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 //config :
 
